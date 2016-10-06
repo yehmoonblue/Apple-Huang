da1a764c0dd28edbcea44db9530ecd93d4259231
nick66551 <john831118@gmail.com>
2016-10-06 14:44:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-var bgImg = document.createElement("img");
-bgImg.src = "images/map.png";
-var buttonImg = document.createElement("img");
-buttonImg.src = "images/tower-btn.png";
-var towerImg = document.createElement("img");
-towerImg.src = "images/tower.png";
-var slimeImg = document.createElement("img");
-slimeImg.src = "images/slime.gif";
-var crosshairImg = document.createElement("img");
-crosshairImg.src = "images/crosshair.png";
-
 
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
-function draw(){
-  
-ctx.drawImage(bgImg,0,0);
+var enemy = {
+  x: 96,
+  y: 480-32
+};
 
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
+var enemyImg = document.createElement("img");
+enemyImg.src = "images/slime.gif";
+
+function draw(){
+  ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(enemyImg, enemy.x, enemy.y);
 }
- setTimeout(draw,1000);
+
+// setTimeout(draw,1000);
+setInterval(draw, 16);
