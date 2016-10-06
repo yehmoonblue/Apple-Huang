8b869b466c2b8344cf6c1bd10b9cd4d7c169317a
nick66551 <john831118@gmail.com>
2016-10-06 14:25:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var buttonImg = document.createElement("img");
+buttonImg.src = "images/tower-btn.png";
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower.png";
+var slimeImg = document.createElement("img");
+slimeImg.src = "images/slime.gif";
+var crosshairImg = document.createElement("img");
+crosshairImg.src = "images/crosshair.png";
+
+
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
