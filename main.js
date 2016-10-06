20b342e6c89dbde90672f452830ed579174d7542
nick66551 <john831118@gmail.com>
2016-10-06 14:57:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/rukia.gif";
+var enemy ={
+  x: 146,
+  y: 480-32
+}
 
 
 function draw(){
-  ctx.drawImage(bgImg,50,20);
+  ctx.drawImage(bgImg,50,0);
   ctx.drawImage(enemyImg,30,30);
 
 }
