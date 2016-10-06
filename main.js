03d1b7ad9766ea4e122ea6143c7ad520d9894f15
nick66551 <john831118@gmail.com>
2016-10-06 14:17:05 +0800

    Add draw function

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+
+function draw(){
+  
+ctx.drawImage(bgImg,0,0);
+
+}
+
+draw();
