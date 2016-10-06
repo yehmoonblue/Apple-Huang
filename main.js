4bf17243b71347daca4a8e48479bdc64a4ca8abd
nick66551 <john831118@gmail.com>
2016-10-06 14:53:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ var ctx = canvas.getContext("2d");
 
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var enemyImg = document.createElement("img");
+enemyImg.src = "images/rukia.gif";
 
 
 function draw(){
   ctx.drawImage(bgImg,50,20);
+  ctx.drawImage(enemyImg,30,30);
 
 }
 setInterval(draw, 16);
