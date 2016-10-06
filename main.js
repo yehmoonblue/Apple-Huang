ab152eccd5ec002db944fd476f1b86194d341840
nick66551 <john831118@gmail.com>
2016-10-06 14:47:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,14 @@
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
-var enemy = {
-  x: 96,
-  y: 480-32
-};
+
 
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
-var enemyImg = document.createElement("img");
-enemyImg.src = "images/slime.gif";
+
 
 function draw(){
   ctx.drawImage(bgImg,20,20);
-  ctx.drawImage(enemyImg, enemy.x, enemy.y);
-}
 
-// setTimeout(draw,1000);
-setInterval(draw, 16);
+}
+ setTimeout(draw,1000);
