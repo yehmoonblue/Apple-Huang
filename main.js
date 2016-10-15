c19cb13f54e81ae5543907fe2d88a09692855068
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 17:27:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 var bglmg = document.createElement("img");
+var rukia = document.createElement("img");
 bglmg.src = "images/map.png";
+rukia.src = "images/rukia.gif";
 function draw(){
   ctx.drawImage(bglmg,0,0);
 }
-var rukia = document.createElement("img");
-rukia.src = "images/rukia.gif";
 setInterval(draw,16);
