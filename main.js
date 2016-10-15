c9ec0906ebd22ae16190c99c36b54d92ac23b7ab
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 17:46:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
-var slime = document.createElement("img");
+var slimeImage = document.createElement("img");
 slime.src = "images/slime.gif";
 var rukia = document.createElement("img");
 rukia.src = "images/rukia.gif";
@@ -12,7 +12,7 @@ var slime = {
   };
 function draw(){
   ctx.drawImage(bglmg,0,0);
-  ctx.drawImage(slime,0,0);
+  ctx.drawImage(slimeImage,0,0);
   ctx.drawImage(rukia,0,0);
 }
 setInterval(draw,16);
