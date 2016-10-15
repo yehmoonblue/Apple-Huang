0d019b4926780a00a8fbaaff014601560f6f2ade
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 17:10:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var ctx = canvas.getContext("2d");
 var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
 function draw(){
-  ctx.drawlmage(bglmg,0,0);
+  ctx.drawImage(bglmg,0,0);
 }
 setInterval(draw,16);
