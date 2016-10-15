4932ec95a39b1a23b33fee852850df29802adc40
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 16:47:28 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var bglmg = document.createElement("img");
+bglmg.src = "images/map.png";
+var canvas = document.getElementByld("game-canvas");
+var ctx = canvas.getContext("2d");
+function draw(){
+ctx.drawlmage(bglmg,0,0);
+}
+draw();
