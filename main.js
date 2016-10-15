59864ab0a5b406c85ced0f92090f433647ebc13d
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 17:52:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
-var enemy = document.createElement("img");
-enemy.src = "images/slime.gif";
+var slime = document.createElement("img");
+slime.src = "images/slime.gif";
 var rukia = document.createElement("img");
 rukia.src = "images/rukia.gif";
-var slime = {
+var enemy = {
   x:96,//x座標
   y:480-32//y座標
   };
 function draw(){
   ctx.drawImage(bglmg,0,0);
-  ctx.drawImage(enemy,0,0);
+  ctx.drawImage(slime,0,0);
   ctx.drawImage(rukia,0,0);
 }
 setInterval(draw,16);
