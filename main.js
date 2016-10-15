901732e9906dd1db905a449a1f8479d2c72c696e
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 17:33:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
 var slime = document.createElement("img");
 slime.src = "images/slime.gif";
+var slime{
+  x:0,
+  y:0
+};
 function draw(){
   ctx.drawImage(bglmg,0,0);
   ctx.drawImage(slime,0,0);
