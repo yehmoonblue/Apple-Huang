1efbc9b114f983b4c085a53bf0334fa88e9b8491
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 17:38:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
 var slime = document.createElement("img");
 slime.src = "images/slime.gif";
+var rukia = document.createElement("img");
+rukia.src = "images/rukia.gif";
+var enemy = {
+  x:96,//x座標
+  y:480-32//y座標
+  };
 function draw(){
   ctx.drawImage(bglmg,0,0);
   ctx.drawImage(slime,0,0);
+  ctx.drawImage(rukia,0,0);
 }
 setInterval(draw,16);
