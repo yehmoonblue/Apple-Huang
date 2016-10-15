76218b5f2398242bdc7d6ae5206f00aae031fa3d
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-15 17:29:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 var bglmg = document.createElement("img");
-var rukia = document.createElement("img");
 bglmg.src = "images/map.png";
-rukia.src = "images/rukia.gif";
+var slime = document.createElement("img");
+slime.src = "images/slime.gif";
 function draw(){
   ctx.drawImage(bglmg,0,0);
 }
