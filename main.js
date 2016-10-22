2dad9005b8c348d9dea7c4e23f85403d97cd745d
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-22 16:16:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var slime = document.createElement("img");
 slime.src = "images/slime.gif";
 var rukia = document.createElement("img");
 rukia.src = "images/rukia.gif";
+var tower = document.createElement("img");
+tower.src = "images/tower-btn.png";
 var enemy = {
   x:96,//x座標
   y:480-32//y座標
@@ -14,5 +16,7 @@ function draw(){
   ctx.drawImage(bglmg,0,0);
   ctx.drawImage(slime,0,0);
   ctx.drawImage(rukia,0,0);
+  ctx.drawImage(tower,100,100);
+  
 }
 setInterval(draw,16);
