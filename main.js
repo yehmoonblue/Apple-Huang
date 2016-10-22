8c0648d8881e2bddafd22a4e89bfe5019a9b4768
nick66551 <john831118@gmail.com>
2016-10-22 12:11:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -35,10 +35,10 @@ function draw(){
   ctx.drawImage(bgImg,50,0);
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
   ctx.drawImage(towerImg,towerbutton.x,towerbutton.y,towerbutton.width,towerbutton.height);
-  if(isBuilding && isCollided(cursor.x, cursor.y, 590, 432, 50, 50)===false){
+  if(isBuilding){
   ctx.drawImage(towerbuiltImg,cursor.x,cursor.y);
   }
-//  ctx.drawImage(towerbuiltImg, tower.x, tower.y);
+  ctx.drawImage(towerbuiltImg, tower.x, tower.y);
 }
 
 //製造城堡
