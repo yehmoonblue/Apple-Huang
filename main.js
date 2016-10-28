7417bc553eeca170a8e9687acd6947a4c73a10a1
yehmoonblue <moonblue.yeh@gmail.com>
2016-10-28 22:25:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
 var slime = document.createElement("img");
 slime.src = "images/slime.gif";
-var enemy = document.createElement("img");
-enemy.src = "images/rukia.gif";
+var rukia = document.createElement("img");
+rukia.src = "images/rukia.gif";
 var tower = document.createElement("img");
 tower.src = "images/tower-btn.png";
 var tower2 = document.createElement("img");
@@ -15,12 +15,6 @@ var cursor = {x:0,y:0};
 var enemy = {
   x:115,//x座標
   y:480-32//y座標
- //  speedx: 0,
-//   speedy: -64,
- //  move: function(){
- //  this.x += this.speedx/FPS;
-//   this.y += this.speedy/FPS;
-//  }
 };
 
 var towerbutton={
@@ -33,7 +27,7 @@ var towerbutton={
 function draw(){
   ctx.drawImage(bglmg,50,0);
   ctx.drawImage(slime,0,0);
-  ctx.drawImage(enemy,enemy.x,enemy.y);
+  ctx.drawImage(rukia,enemy.x,enemy.y);
   ctx.drawImage(tower,towerbutton.x,towerbutton.y,towerbutton.width,towerbutton.height);
   if(isBuilding){
       ctx.drawImage(towerbuiltImg,cursor.x,cursor.y);
