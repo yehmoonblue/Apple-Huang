2e0d4e1ba1956ff9cedc2804ab00a85224901092
yehmoonblue <moonblue.yeh@gmail.com>
2016-11-05 17:11:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -37,25 +37,7 @@ var enemy = {
       this.x,this.y,
       this.pathDes++;
       console.log("qq")
-    if(this.x>enemyPath[this.pathDes].x){
-      this.speedx=-64;
-      this.speedy = 0;
-    } 
-      else if(this.x<enemyPath[this.pathDes].x){
-      this.speedx=64;
-      this.speedy=0;
-    }
-      else if(this.y<enemyPath[this.pathDes].y){
-      this.speedx=0;
-      this.speedy=64;
-    }
-      else{
-      this.speedx=0;
-      this.speedy=-64;
-      }}
-      else{
-    this.x=this.x+this.speedx/FPS;
-    this.y=this.y+this.speedy/FPS;
+    
       }
   }
 };
