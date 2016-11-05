7015a3ff245fa03e0989d7e2d0d9a8d7759cba93
yehmoonblue <moonblue.yeh@gmail.com>
2016-11-05 16:26:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ var tower2 = document.createElement("img");
 tower2.src = "images/tower.png";
 var cursor = {x:0,y:0};
 var enemy = {
-  x:400,
-  y:448,
+  x:350,
+  y:60,
   //{x:96,y:60},
     //{x:400,y:60}
   speedx :0,
