3b916e8f3a28a369ed28751042271c4094df4f8b
yehmoonblue <moonblue.yeh@gmail.com>
2016-11-12 17:12:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -75,6 +75,10 @@ var towerbutton={
 
 
 function draw(){
+  for(var i=0;i<enemies.length;i++){
+    enemies[i].move();
+    ctx.drawImage(rukia,enemies[i].x,enemies[i].y);
+  }
   if (clock%80==0){
     var newEnemy = new Enemy();
     enemies.push(newEnemy);
