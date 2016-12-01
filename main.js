239aceee2178696d5bc946d59e021ccba0bea273
yehmoonblue <moonblue.yeh@gmail.com>
2016-12-01 20:21:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ var towerbutton={
 
 function draw(){
     ctx.drawImage(bgImg,0,0);
-    ctx.drawImage(green,0,0,20,20);
+    ctx.drawImage(green,0,0,30,40);
     if(clock%80==0){
         var newenemy= new Enemy();
         enemies.push(newenemy);
